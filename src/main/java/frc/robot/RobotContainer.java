da135c44b3cdf695cf2363a85f2d0bf1e7f9c23a
Joshua Allard <jnallard@users.noreply.github.com>
2023-01-09 21:38:30 -0500

    Testing that shared code and gamepads work. We will want the students to do this themselves

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,13 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import com.chaos131.gamepads.Gamepad;
+
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -23,8 +29,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final Gamepad m_driverController =
+      new Gamepad(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -48,7 +54,9 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_driverController.b().whileTrue(new RunCommand(() -> m_driverController.getHID().setRumble(RumbleType.kBothRumble, 0.5)));
+    m_driverController.b().whileFalse(new RunCommand(() -> m_driverController.getHID().setRumble(RumbleType.kBothRumble, 0.0)));
+    Shuffleboard.getTab("Test").addBoolean("b pressed", () -> m_driverController.b().getAsBoolean());
   }
 
   /**

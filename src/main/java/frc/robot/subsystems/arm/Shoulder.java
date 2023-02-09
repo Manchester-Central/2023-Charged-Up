e03e28e973c6852e5082910e69471599560c56da
NinjaBuilderPro <NinjaBuilderPro@users.noreply.github.com>
2023-02-08 20:16:10 -0500

    Added PID tuner

--- src/main/java/frc/robot/subsystems/arm/Shoulder.java
+++ src/main/java/frc/robot/subsystems/arm/Shoulder.java
@@ -4,11 +4,15 @@
 
 package frc.robot.subsystems.arm;
 
+import com.chaos131.pid.PIDTuner;
+import com.chaos131.pid.PIDUpdate;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.Constants.ArmConstants;
 
@@ -19,6 +23,7 @@ public class Shoulder {
     CANSparkMax m_shoulderR_A;
     CANSparkMax m_shoulderR_B;
     SparkMaxAbsoluteEncoder m_AbsoluteEncoder;
+    PIDTuner m_PidTuner;
 
     public Shoulder (){
         m_shoulderL_A = new CANSparkMax(ArmConstants.CanIdShoulderL_A, MotorType.kBrushless);
@@ -30,9 +35,27 @@ public class Shoulder {
         m_AbsoluteEncoder = m_shoulderL_A.getAbsoluteEncoder(Type.kDutyCycle);
         m_AbsoluteEncoder.setPositionConversionFactor(ArmConstants.ShoulderAngleConversionFactor);
         m_AbsoluteEncoder.setZeroOffset(ArmConstants.ShoulderAngleZeroOffset);
+        m_PidTuner = new PIDTuner("ShoulderPID", true, 0.0001, 0, 0, this::tunePID);
     }
 
     public Rotation2d getRotation(){
         return Rotation2d.fromDegrees(m_AbsoluteEncoder.getPosition());
+    }
+
+    public void setTargetAngle(){
+
+    }
+
+    public void tunePID(PIDUpdate pidUpdate){
+        setPID(pidUpdate, m_shoulderL_A.getPIDController());
+        setPID(pidUpdate, m_shoulderL_B.getPIDController());
+        setPID(pidUpdate, m_shoulderR_A.getPIDController());
+        setPID(pidUpdate, m_shoulderR_B.getPIDController());
+    }
+
+    public void setPID(PIDUpdate pidUpdate, SparkMaxPIDController controller){
+        controller.setP(pidUpdate.P);
+        controller.setI(pidUpdate.I);
+        controller.setD(pidUpdate.D);
     }
 }

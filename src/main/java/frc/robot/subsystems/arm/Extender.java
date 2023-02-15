fcce2c87fc57e84f53b8e732321203c433171697
huyha23 <huyha23@users.noreply.github.com>
2023-02-14 20:01:45 -0500

    Added PID tuner.

--- src/main/java/frc/robot/subsystems/arm/Extender.java
+++ src/main/java/frc/robot/subsystems/arm/Extender.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems.arm;
 
+import com.chaos131.pid.PIDTuner;
+import com.chaos131.pid.PIDUpdate;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -12,8 +14,10 @@ import frc.robot.Constants.ArmConstants;
 /** Add your docs here. */
 public class Extender {
     CANSparkMax m_SparkMax;
+    private PIDTuner pidTuner;
     public Extender(){
         m_SparkMax = new CANSparkMax(ArmConstants.CanIdExtender, MotorType.kBrushless);
+        pidTuner = new PIDTuner("ExtenderPID", true, 0.09, 0, 0, this::tunePID);
     }
 
     public void ExtendToTarget(double targetPosition){
@@ -22,5 +26,12 @@ public class Extender {
 
     public double getPosition(){
         return 0.0; //TODO not real value
+    }
+
+    public void tunePID(PIDUpdate pidUpdate){
+        m_SparkMax.getPIDController().setP(pidUpdate.P);
+        m_SparkMax.getPIDController().setI(pidUpdate.I);        
+        m_SparkMax.getPIDController().setD(pidUpdate.D);
+        m_SparkMax.getPIDController().setFF(pidUpdate.F);
     }
 }

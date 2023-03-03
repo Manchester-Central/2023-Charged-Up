c755a7d844ec62e1ca6dcaca7f4a625ab77da5d4
jnallard <joshuaal2000@gmail.com>
2023-03-02 21:13:26 -0500

    Adds filters to driver joysticks

---
+++ src/main/java/frc/robot/commands/BaseJoystickDrive.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import com.chaos131.gamepads.Gamepad;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.swerve.SwerveDrive;
+
+public abstract class BaseJoystickDrive extends CommandBase {
+
+  public static boolean UseSlewer = false;
+
+  protected interface JoystickFilter {
+    double get();
+  }
+
+  protected class JoystickSlewer implements JoystickFilter {
+    SlewRateLimiter m_slewRateLimiter = new SlewRateLimiter(6); // Test for a good value. This is amount of input change in one second, so should allow 0 to full in 1/6 a second
+    Supplier<Double> m_joystickSupplier;
+    
+    JoystickSlewer(Supplier<Double> joystickSupplier) {
+      m_joystickSupplier = joystickSupplier;
+    }
+
+    public double get() {
+      return m_slewRateLimiter.calculate(m_joystickSupplier.get());
+    }
+  }
+
+  protected class JoystickLowPassFilter implements JoystickFilter {
+    double lastValue = 0;
+    Supplier<Double> m_joystickSupplier;
+
+    JoystickLowPassFilter(Supplier<Double> joystickSupplier) {
+      m_joystickSupplier = joystickSupplier;
+    }
+
+    public double get() {
+      double newValue = (m_joystickSupplier.get() + lastValue) / 2;
+      lastValue = newValue;
+      return newValue;
+    }
+  }
+
+  protected final SwerveDrive m_swerveDrive;
+  protected final Gamepad m_driverController;
+  protected final JoystickFilter m_slewedLeftX;
+  protected final JoystickFilter m_slewedLeftY;
+  protected final JoystickFilter m_slewedRightX;
+  protected final JoystickFilter m_slewedRightY;
+
+  /** Shares some code for all joystick drives */
+  public BaseJoystickDrive(SwerveDrive swerveDrive, Gamepad driverController) {
+    m_swerveDrive = swerveDrive;
+    m_driverController = driverController;
+    if (UseSlewer) {
+      m_slewedLeftX = new JoystickSlewer(() -> driverController.getLeftX());
+      m_slewedLeftY = new JoystickSlewer(() -> driverController.getLeftY());
+      m_slewedRightX = new JoystickSlewer(() -> driverController.getRightX());
+      m_slewedRightY = new JoystickSlewer(() -> driverController.getRightY());
+    } else {
+      m_slewedLeftX = new JoystickLowPassFilter(() -> driverController.getLeftX());
+      m_slewedLeftY = new JoystickLowPassFilter(() -> driverController.getLeftY());
+      m_slewedRightX = new JoystickLowPassFilter(() -> driverController.getRightX());
+      m_slewedRightY = new JoystickLowPassFilter(() -> driverController.getRightY());
+    }
+
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(swerveDrive);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_swerveDrive.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

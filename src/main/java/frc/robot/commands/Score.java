961f6dbc5e63911c2c5bf7e47216decbe7fc093a
Kenneth Olibrice <kenneth.olibrice@gmail.com>
2023-03-25 16:28:36 -0400

    After match 38

--- src/main/java/frc/robot/commands/Score.java
+++ src/main/java/frc/robot/commands/Score.java
@@ -68,7 +68,7 @@ public class Score extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_gripper.setGripperMode(GripperMode.hold);
+    m_gripper.setGripperMode(GripperMode.unGrip);
   }
 
   // Returns true when the command should end.

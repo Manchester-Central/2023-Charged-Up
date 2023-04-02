241b476c96aadfda181f259b212ea90efa115d6f
Kenneth Olibrice <100235157+Kenneth-Olibrice@users.noreply.github.com>
2023-04-02 16:26:59 -0400

    Changed to WPILib AddressableLED class for led implementation.

--- src/main/java/frc/robot/subsystems/ArduinoIO.java
+++ src/main/java/frc/robot/subsystems/ArduinoIO.java
@@ -1,57 +1,33 @@
 package frc.robot.subsystems;
 
 
-import com.fazecast.jSerialComm.SerialPort;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArduinoIO extends SubsystemBase {
-    private SerialPort m_arduinoPort;
-    private String m_rgbMessage;
+    private AddressableLED m_lightStrip;
+    private AddressableLEDBuffer m_buffer;
     private Object m_mutex = new Object();
 
     public ArduinoIO() {
-        setRGB(255, 255, 255);
-        try {
-            SerialPort[] ports = SerialPort.getCommPorts();
-            for(int i = 0; i < ports.length; i++) {
-              //  SmartDashboard.putString("Port path " + Integer.toString(i), ports[i].getSystemPortPath());
-              //  SmartDashboard.putString("Port name " + Integer.toString(i), ports[i].getDescriptivePortName());
-            }
-            m_arduinoPort = ports[3];
-            m_arduinoPort.openPort();
-            
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        m_buffer = new AddressableLEDBuffer(30);
+        setRGB(200, 200, 200);
+        m_lightStrip = new AddressableLED(2);
+        m_lightStrip.setLength(m_buffer.getLength());
+        m_lightStrip.setData(m_buffer);
+        m_lightStrip.start();
     }
     
     @Override
     public void periodic() {
-        synchronized(m_mutex) {
-            try {
-                    SmartDashboard.putBoolean("isPortOpen", m_arduinoPort.isOpen());
-                    m_arduinoPort.writeBytes(m_rgbMessage.getBytes(), m_rgbMessage.length());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     public void setRGB(int red, int green,  int blue) {  
-        synchronized(m_mutex) {
-            String redString = String.format("%03d", red);
-            String greenString = String.format("%03d", green);
-            String blueString = String.format("%03d", blue);
-            m_rgbMessage = redString + greenString + blueString + ";\n";
-            SmartDashboard.putString("rgb", m_rgbMessage);
-        }
+            for(int i = 0; i < m_buffer.getLength(); i++) {
+                m_buffer.setRGB(i, red, green, blue);
+            }
     }
-
-   
-
-
 }
 
 
